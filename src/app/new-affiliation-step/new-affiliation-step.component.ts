7a1af352dd68cee449b6357dd43c3f668a8a6ac6
Arnold B. Costamero <a012001164@HO.AD.BDO>
2018-08-07 19:37:30 +0800

    Commit 08072018

---
+++ src/app/new-affiliation-step/new-affiliation-step.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder,FormGroup,Validators} from '@angular/forms';
+import { FormlyFormOptions, FormlyFieldConfig } from '@ngx-formly/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+@Component({
+  selector: 'app-new-affiliation-step',
+  templateUrl: './new-affiliation-step.component.html',
+  styleUrls: ['./new-affiliation-step.component.css']
+})
+export class NewAffiliationStepComponent implements OnInit {
+  isLinear = false;
+	isOptional= false;
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+  title = 'New Affiliation';
+   subTitle = 'Create';
+   mode='create';
+   status="done";
+  constructor(private _formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+
+    this.isOptional= true;
+  	this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
+  }
+
+}
