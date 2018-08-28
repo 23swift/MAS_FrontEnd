399732ff47d386201f3d1dd09acddbf724daf391
Jonas Reuben V. Navarro <a025015373@HO.AD.BDO>
2018-08-28 18:01:34 +0800

    maef fields 08282018

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -39,12 +39,6 @@ import { NewAffiliationStepComponent } from './new-affiliation-step/new-affiliat
 import { HomeScreenComponent } from './home-screen/home-screen.component';
 import { OcularInspectionFormComponent } from './ocular-inspection-form/ocular-inspection-form.component';
 import { DocumentCheckListComponent } from './document-check-list/document-check-list.component';
-<<<<<<< HEAD
-import { ServiceFeeContractFormComponent } from './ServiceFeeContract/service-fee-contract-form/service-fee-contract-form.component';
-import { ServiceFeeContractListComponent } from './ServiceFeeContract/service-fee-contract-list/service-fee-contract-list.component';
-import { ServiceFeeContractComponent } from './ServiceFeeContract/service-fee-contract/service-fee-contract.component';
-import { MaefComponent } from './maef/maef.component';
-=======
 import { NewAffiliationSumComponent } from './new-affiliation-sum/new-affiliation-sum.component';
 import { CustomerProfileComponent } from './customer-profile/customer-profile.component';
 import { BranchListAttachmentComponent } from './branch-list-attachment/branch-list-attachment.component';
@@ -55,8 +49,8 @@ import { RequestHeaderComponent } from './request-header/request-header.componen
 import { MaefFieldsComponent } from './maef-fields/maef-fields.component';
 import { AwrFormComponent } from './forms/awr-form/awr-form.component';
 import { RequestFormComponent } from './forms/request-form/request-form.component';
+import { MaefComponent } from './maef/maef.component';
 
->>>>>>> b8a9e169bced98113f6cd746ad6229864fdf9d36
 
 @NgModule({
   declarations: [
@@ -79,12 +73,6 @@ import { RequestFormComponent } from './forms/request-form/request-form.componen
     HomeScreenComponent,
     OcularInspectionFormComponent,
     DocumentCheckListComponent,
-<<<<<<< HEAD
-    ServiceFeeContractFormComponent,
-    ServiceFeeContractListComponent,
-    ServiceFeeContractComponent,
-    MaefComponent
-=======
     NewAffiliationSumComponent,
     CustomerProfileComponent,
     BranchListAttachmentComponent,
@@ -94,8 +82,8 @@ import { RequestFormComponent } from './forms/request-form/request-form.componen
     RequestHeaderComponent,
     MaefFieldsComponent,
     AwrFormComponent,
-    RequestFormComponent
->>>>>>> b8a9e169bced98113f6cd746ad6229864fdf9d36
+    RequestFormComponent,
+    MaefComponent
   ],
   imports: [
     BrowserModule,
--- src/app/maef/maef.component.ts
+++ src/app/maef/maef.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {FormGroup} from '@angular/forms';
+import { FormlyFormOptions, FormlyFieldConfig } from '@ngx-formly/core';
+import{MaefService} from './maef.service';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { AppBaseComponent } from '../app-base/app-base.component';
 
 @Component({
   selector: 'app-maef',
   templateUrl: './maef.component.html',
   styleUrls: ['./maef.component.css']
 })
-export class MaefComponent implements OnInit {
-
-  constructor() { }
+export class MaefComponent extends AppBaseComponent implements OnInit {
+  form = new FormGroup({});
+  model: any = {};
+  options: FormlyFormOptions = {};
+  fields: FormlyFieldConfig[];
+  title :string='MAEF';
+  subTitle:string='';
+  mode:string;
+  private _maefService:MaefService;
+  constructor( private maefService:MaefService,public route: ActivatedRoute,
+    public router: Router) {
+      super(route,router);
+      this._maefService=maefService;
+      this.getFields();
+     }
 
   ngOnInit() {
+    this.title='Branch';
+    // this.baseCreateFunction=this.create;
+    // this.baseUpdateFunction=this.update;
+   
+    this.initialize();
+  }
+
+  public create(){
+    alert('Create branch');
+  }
+
+  public update(){alert('Update Branch');
+      alert(this.route.snapshot.paramMap.get('id'));
+}
+  
+  public cancel(){  
+    this.router.navigateByUrl('/naStep/create');
+  }
+ 
+
+  public getFields() {
+    this.fields=this._maefService.getMaefFields();
   }
 
 }
