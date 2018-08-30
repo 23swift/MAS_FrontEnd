b265fc0e80a3cfb688880bcf7851d8e20aae39e0
Martin Magpantay Jr <martinboy.magpantayjr@gmail.com>
2018-08-31 01:52:43 +0800

    Updated MID Service - Coco08312018

--- src/app/mid-request/mid-request.component.ts
+++ src/app/mid-request/mid-request.component.ts
@@ -7,12 +7,12 @@ export interface MidRequest {
   sfcc: string;
 }
 const ELEMENT_DATA: MidRequest[] = [
-  {currency: 'PHP', monitorCode: 'OTC',merchantGroupCode:'MerchGrp4', sfcc: 'H'},
-  {currency: 'USD', monitorCode: 'Reg Inst',merchantGroupCode:'MerchGrp1Z', sfcc: 'He'},
-  {currency: 'PHP', monitorCode: '0% Inst',merchantGroupCode:'', sfcc: 'Li'},
-  {currency: 'PHP', monitorCode: 'BNPL Reg',merchantGroupCode:'MerchGrp4', sfcc: 'Be'},
-  {currency: 'PHP', monitorCode: 'BNPL 0%',merchantGroupCode:'MerchGrp1Z', sfcc: 'B'}
-  
+  { currency: 'PHP', monitorCode: 'OTC', merchantGroupCode: 'MerchGrp4', sfcc: 'H' },
+  { currency: 'USD', monitorCode: 'Reg Inst', merchantGroupCode: 'MerchGrp1Z', sfcc: 'He' },
+  { currency: 'PHP', monitorCode: '0% Inst', merchantGroupCode: '', sfcc: 'Li' },
+  { currency: 'PHP', monitorCode: 'BNPL Reg', merchantGroupCode: 'MerchGrp4', sfcc: 'Be' },
+  { currency: 'PHP', monitorCode: 'BNPL 0%', merchantGroupCode: 'MerchGrp1Z', sfcc: 'B' }
+
   // {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
   // {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
   // {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
@@ -25,7 +25,7 @@ const ELEMENT_DATA: MidRequest[] = [
 })
 export class MidRequestComponent implements OnInit {
 
-displayedColumns: string[] = ['currency', 'monitorCode', 'merchantGroupCode', 'sfcc','controls'];
+  displayedColumns: string[] = ['currency', 'monitorCode', 'merchantGroupCode', 'sfcc', 'controls'];
   dataSource = ELEMENT_DATA;
   constructor() { }
 
