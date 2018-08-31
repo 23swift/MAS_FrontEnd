da461826f2ec65399653ba2e503505f56688b124
Martin Magpantay Jr <martinboy.magpantayjr@gmail.com>
2018-08-31 12:30:16 +0800

    Add MID Component - Coco08312018

---
+++ src/app/mid/mid.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MidService } from './mid.service';
+
+@Component({
+  selector: 'app-mid',
+  templateUrl: './mid.component.html',
+  styleUrls: ['./mid.component.css'],
+  providers: [MidService]
+})
+export class MidComponent implements OnInit {
+  displayedColumns: string[];
+  mode: string;
+  dataSource;
+  constructor(private _route: ActivatedRoute, private _router: Router, private _service: MidService) { }
+
+  ngOnInit() {
+    this.dataSource = this._service.Get();
+    this.displayedColumns = this._service.GetTableFields();
+  }
+  AddMid() {
+    this._router.navigateByUrl('/midForm');
+  }
+}
